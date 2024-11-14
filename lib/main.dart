544a7121d3310655f74370da7ac17fa8e63d7d6d
rifqidaiva <rivkidaiva123@gmail.com>
2024-11-14 16:40:20 +0700

    Create app layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pam_final_client/pages/page_chats.dart';
-import 'package:pam_final_client/pages/page_conversation.dart';
 import 'package:pam_final_client/pages/page_login.dart';
-import 'package:pam_final_client/pages/page_profile.dart';
-import 'package:pam_final_client/pages/page_register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,51 +24,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       themeMode: ThemeMode.light,
-      home: const Page(),
-    );
-  }
-}
-
-class Page extends StatelessWidget {
-  const Page({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ListView(
-          children: const [
-            Option(title: "Login", page: Pagelogin()),
-            Option(title: "Register", page: PageRegister()),
-            Option(title: "Conversation", page: PageConversation()),
-            Option(title: "Chats", page: PageChats()),
-            Option(title: "Profile", page: PageProfile()),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Option extends StatelessWidget {
-  final String title;
-  final Widget page;
-
-  const Option({super.key, required this.title, required this.page});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      trailing: const Icon(Icons.arrow_forward),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => page,
-          ),
-        );
-      },
+      home: const Pagelogin(),
     );
   }
 }
